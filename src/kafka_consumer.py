ae8e8c75459e86b0fc3f793f67724f0cc813c9db
Pierre-Alexandre Baril <pierrealexstyle@gmail.com>
2024-10-25 16:54:45 +0000

    [update] consumer uploads to blob storage

--- src/kafka_consumer.py
+++ src/kafka_consumer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 from confluent_kafka import Consumer, KafkaException, KafkaError
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
 import json
+import time
 
 # example url: localhost:45983
 KAFKA_SERVER_URL = 'localhost:29092'
 TOPIC = "live_flight_positions_full_france"
+AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
 # Define the JSON deserializer
 def json_deserializer(value):
@@ -25,6 +29,15 @@ consumer = Consumer(consumer_conf)
 # Subscribe to the topic
 consumer.subscribe([TOPIC])
 
+# Connect to your Azure Blob Storage account
+blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+container_name = "flightdata"
+# Create the container
+# container_client = blob_service_client.create_container(container_name)
+# time.sleep(10)
+# Get a reference to the container
+container_client = blob_service_client.get_container_client(container_name)
+
 try:
     while True:
         msg = consumer.poll(timeout=1.0)
@@ -44,6 +57,10 @@ try:
             # Deserialize the JSON message
             data = json_deserializer(msg.value())
             print(f'Received message: data: {data}')
+            # Process messages and upload to Azure Blob Storage
+            blob_name = f"flight_info_{msg.key().decode('utf-8')}.json"
+            blob_client = container_client.get_blob_client(blob=blob_name)
+            blob_client.upload_blob(msg.value(), overwrite=False)
 
 except KeyboardInterrupt:
     pass
